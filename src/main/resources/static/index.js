1f799b56e5501daebc89ac2089a15e4ff2faee79
Karol Bułdak <s221451@student.uek.krakow.pl>
2023-11-29 17:36:23 +0100

    Added button that saves the note to database

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,4 +1,28 @@
+document.getElementById('saveButton').addEventListener('click', postNote);
+
 tinymce.init({
     selector: 'textarea#tiny',
     skin: "bootstrap"
 });
+
+function getContent(){
+    return tinymce.get('tiny').getContent()
+}
+
+function postNote(){
+    const content = getContent();
+    fetch('http://localhost:8080/notes', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                "author":"test",
+                "content":content
+            })
+    })
+        .then(response => response.json())
+        .then(response => console.log(JSON.stringify(response)))
+}
