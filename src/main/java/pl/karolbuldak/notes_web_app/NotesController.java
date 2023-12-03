9a26c4c7fcf35a50485e2a664394c67e37ce5acd
Karol Bułdak <s221451@student.uek.krakow.pl>
2023-12-03 13:04:47 +0100

    Changed link to API

--- src/main/java/pl/karolbuldak/notes_web_app/NotesController.java
+++ src/main/java/pl/karolbuldak/notes_web_app/NotesController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/notes")
+@RequestMapping("/api")
 public class NotesController {
 
     @Autowired
